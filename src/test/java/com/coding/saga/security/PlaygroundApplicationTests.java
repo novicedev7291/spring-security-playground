9606ae52a029231522a9401002a1336fe8cd3865
kuldeep <kuldeep.yadav@noosyntech.in>
2021-03-26 23:07:09 +0530

    Initialize

---
+++ src/test/java/com/coding/saga/security/PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.saga.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
